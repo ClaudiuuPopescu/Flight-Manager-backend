015987193971f0f2222fb07cc24649026add0ce1
popesc <Claudiu-Sergiu.Popescu@msg.group>
2023-02-27 13:19:50 +0200

    Update CompanyDTO.java

--- flight-manager/src/main/java/dto/CompanyDTO.java
+++ flight-manager/src/main/java/dto/CompanyDTO.java
@@ -1,27 +1,5 @@
 package dto;
 
-import java.sql.Date;
-
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@Builder(toBuilder = true)
-@NoArgsConstructor
-@AllArgsConstructor
-public class CompanyDTO {
-
-
-	private Long idCompany;
-
-	private String name;
-    
-	private String phoneNumber;
-    
-	private String email;
-
-	private Date foundedIn;
+public class CompanyDto {
 
 }
