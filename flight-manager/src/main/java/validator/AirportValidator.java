50bc382250b2dcc48f9cb9e0810a2140f3cc931b
DariaRadu2001 <dariaradu20@yahoo.com>
2023-03-01 11:03:24 +0200

    AirportValidator done

--- flight-manager/src/main/java/validator/AirportValidator.java
+++ flight-manager/src/main/java/validator/AirportValidator.java
@@ -1,13 +1,40 @@
 package validator;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import dto.AddressDto;
 import dto.AirportDto;
+import exceptions.ErrorCode;
+import exceptions.ValidatorException;
 
 @Component
 public class AirportValidator {
+	
+	@Autowired
+	private AddressValidator addressValidator;
 
-	public void validateAirport(AirportDto airportDto) {
-		
+	public void validateAirport(AirportDto airportDto) throws ValidatorException {
+
+		validateAddress(airportDto.getAddressDto());
+		validateAirportName(airportDto.getAirportName());
+	}
+
+	private void validateAddress(AddressDto addressDto) throws ValidatorException {
+		addressValidator.validateAddress(addressDto);
+	}
+
+	private void validateAirportName(String airportName) throws ValidatorException {
+
+		if (airportName.isEmpty())
+			throw new ValidatorException("The airport name cannot be empty!", ErrorCode.EMPTY_FIELD);
+
+		if (airportName.length() > 30)
+			throw new ValidatorException("The airport name is too long!", ErrorCode.IS_TOO_LONG);
+
+		if (!StringUtils.isAsciiPrintable(airportName))
+			throw new ValidatorException("The airport name should be only out of letters!",
+					ErrorCode.IS_NOT_OUT_OF_LETTERS);
+
 	}
 }
