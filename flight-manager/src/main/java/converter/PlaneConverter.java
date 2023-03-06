a2b7b5fec1713d21879b7a23609b563aaedd548c
DariaRadu2001 <dariaradu20@yahoo.com>
2023-03-06 11:11:01 +0200

    Merge branch 'main' into feature/flightTemplateService-CRUD

--- flight-manager/src/main/java/converter/PlaneConverter.java
+++ flight-manager/src/main/java/converter/PlaneConverter.java
@@ -3,10 +3,8 @@ package converter;
 import org.springframework.stereotype.Component;
 
 import dto.PlaneDto;
-<<<<<<< HEAD
-=======
 import modelHelper.CreatePlaneModel;
->>>>>>> main
+
 import msg.project.flightmanager.model.Plane;
 
 @Component
@@ -14,17 +12,13 @@ public class PlaneConverter implements IConverter<Plane, PlaneDto> {
 
 	@Override
 	public PlaneDto convertToDTO(Plane plane) {
-<<<<<<< HEAD
-	
-		return null;
-=======
+
 		PlaneDto planeDto = PlaneDto.builder().model(plane.getModel()).tailNumber(plane.getTailNumber())
 				.capacity(plane.getCapacity()).fuelTankCapacity(plane.getFuelTankCapacity())
 				.manufacturingDate(plane.getManufacturingDate()).firstFlight(plane.getFirstFlight())
 				.lastRevision(plane.getLastRevision()).size(plane.getSize()).build();
 
 		return planeDto;
->>>>>>> main
 	}
 
 	@Override
@@ -33,9 +27,6 @@ public class PlaneConverter implements IConverter<Plane, PlaneDto> {
 		return null;
 	}
 
-<<<<<<< HEAD
-}
-=======
 	public Plane createModelToEntity(CreatePlaneModel createPlaneModel) {
 
 		Plane plane = Plane.builder().model(createPlaneModel.getModel()).tailNumber(createPlaneModel.getTailNumber())
@@ -45,4 +36,3 @@ public class PlaneConverter implements IConverter<Plane, PlaneDto> {
 		return plane;
 	}
 }
->>>>>>> main
