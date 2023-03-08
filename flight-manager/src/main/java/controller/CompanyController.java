79de53da907ffffc510616f6bb1d8fbd9fafd50d
DariaRadu2001 <dariaradu20@yahoo.com>
2023-03-08 11:17:19 +0200

    yes

--- flight-manager/src/main/java/controller/CompanyController.java
+++ flight-manager/src/main/java/controller/CompanyController.java
@@ -1,8 +1,23 @@
 package controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
+
+import service.interfaces.ICompanyService;
+import service.interfaces.IUserService;
 
 @RestController
 public class CompanyController {
+	private final ICompanyService companyService;
+	
+	private final IUserService userService;
+	
+    @Autowired
+    public CompanyController(ICompanyService companyService, IUserService userService) {
+        this.companyService = companyService;
+		this.userService = userService;
+    }
+    
+    
 
 }
