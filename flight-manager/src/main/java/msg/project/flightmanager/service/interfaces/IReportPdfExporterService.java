e5e45d50564415680adb177fe6754b1e1c76a872
claudiupopescu <Claudiu-Sergiu.Popescu@msg.group>
2023-04-13 13:03:09 +0300

    feature/report-controller-exportPdfRestApi

--- flight-manager/src/main/java/msg/project/flightmanager/service/interfaces/IReportPdfExporterService.java
+++ flight-manager/src/main/java/msg/project/flightmanager/service/interfaces/IReportPdfExporterService.java
@@ -1,8 +1,6 @@
 package msg.project.flightmanager.service.interfaces;
 
-import msg.project.flightmanager.model.Report;
-
 public interface IReportPdfExporterService {
 	
-	void exportToPdf(Report report);
+	void exportToPdf(String reportCode);
 }
